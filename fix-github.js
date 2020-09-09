8d749522b371cde8b141c2b73b0339fa8beb62a9
nicolaferraro <ni.ferraro@gmail.com>
2020-09-09 21:54:17 +0200

    Fix workflow

--- fix-github.js
+++ fix-github.js
@@ -3,3 +3,9 @@ const path = require('path');
 const e404 = path.resolve(__dirname, 'src/404.html');
 const targetFilePath = path.resolve(__dirname, 'dist/404.html');
 fs.createReadStream(e404).pipe(fs.createWriteStream(targetFilePath));
+
+const cname = "kamelets.nicolaferraro.me";
+const targetCNAME = path.resolve(__dirname, 'dist/CNAME');
+const streamCNAME = fs.createWriteStream(targetCNAME);
+streamCNAME.write(cname);
+streamCNAME.end();
